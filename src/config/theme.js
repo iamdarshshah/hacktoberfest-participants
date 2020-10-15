786bf58458f2e15d41b009bb06198cb37142c30f
Ayush Srivastava <36740639+ayush015@users.noreply.github.com>
2020-10-15 16:08:36 +0530

    added the google font Ubuntu (#107)
    * added myslef to contributers list
    
    * updated the version gatsby-transformer-json
    
    * version upgrade
    
    * added new font Ubuntu
    
    * added more font
    
    * added more fonts
    
    * added fonts to theme array
    
    * added fonts to theme
    
    * merge conflict

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,7 +1,7 @@
 import { createMuiTheme } from '@material-ui/core'
 
 const typography = {
-  fontFamily: ['Helvetica'].join(','),
+  fontFamily: ['Ubuntu','Frank Ruhl Libre','Yatra One','Rakkas'].join(','),
 }
 
 const breakpoints = {
